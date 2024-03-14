2df163935ce809a07520595130587c91d29691be
pratibh13 <pratibh.goshi13@gmail.com>
2024-03-14 11:37:40 +0000

    updated code

--- app/__init__.py
+++ app/__init__.py
@@ -2,9 +2,9 @@ from flask import Flask
 app = Flask(__name__)
 
 from app.Database.database import db
-import config
-app.config['SQLALCHEMY_DATABASE_URI'] = config.SQLALCHEMY_DATABASE_URI
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = config.SQLALCHEMY_TRACK_MODIFICATIONS
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:root@db/projects'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db.init_app(app)
     
 from app.Routes import routes
--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 # Database Configuration
-SQLALCHEMY_DATABASE_URI='mysql://root:root@localhost:3306/projects'
+SQLALCHEMY_DATABASE_URI='mysql://root:root@db/projects'
 
 SQLALCHEMY_TRACK_MODIFICATIONS=False
 
