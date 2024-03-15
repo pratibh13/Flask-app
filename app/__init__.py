6bd7643121ace1205e55a1db50ac16f0507cb1f1
Rahul Vishwakarma <rahulvishwakarma1181@gmail.com>
2024-03-15 05:45:31 +0000

    updates

--- app/__init__.py
+++ app/__init__.py
@@ -2,9 +2,9 @@ from flask import Flask
 app = Flask(__name__)
 
 from app.Database.database import db
-
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:root@db/projects'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+import config
+app.config['SQLALCHEMY_DATABASE_URI'] = config.SQLALCHEMY_DATABASE_URI
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = config.SQLALCHEMY_TRACK_MODIFICATIONS
 db.init_app(app)
     
 from app.Routes import routes
